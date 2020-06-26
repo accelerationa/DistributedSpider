95c237f04aafe0cc65625a4e7fe9f11ec857f235
accelerationa <yongcong.luo@gmail.com>
2020-06-25 20:53:10 -0700

    fix bug

--- src/cloudwatch_agent.py
+++ src/cloudwatch_agent.py
@@ -4,9 +4,9 @@ class CloudWatchAgent:
     def __init__(self):
         self.client = boto3.client('cloudwatch', 'us-west-2')
 
-    def put_latency_metrics(self, latecny, worker_ip, spider_name):
+    def put_latency_metrics(self, latency, worker_ip, spider_name):
         metrics = {
-            'MetricName': 'ActionLatecny',
+            'MetricName': 'ActionLatency',
             'Dimensions': [
                 {
                     'Name': 'WorkerNodeIp',
@@ -18,9 +18,9 @@ class CloudWatchAgent:
             'Unit': 'Seconds',
         }
 
-        metrics['Value'] = latecny
+        metrics['Value'] = latency
         metrics['Dimensions'][0]['Value'] = worker_ip
-        metrics['Dimensions'][1]['Value'] = spider_name
+        metrics['Dimensions'][1]['Value'] = str(spider_name)
 
         self.client.put_metric_data(
             MetricData=[metrics],
