ea497209cd85b9584cc7bd2fc633ab286b9738dc
accelerationa <yongcong.luo@gmail.com>
2020-06-28 21:57:15 -0700

    fixing test entry generator bug

--- src/test_task_entry_generator.py
+++ src/test_task_entry_generator.py
@@ -13,18 +13,13 @@ def generate_test_entries(num_entries, database, table, use_local_database):
     for i in range(num_entries):
         dao.newTask(url=str(uuid.uuid4()))
 
-if __name__ == "__main__":
-    generate_test_entries(10, database="SpiderTaskQueue", table="SpiderTaskQueue", use_local_database = True)
-
-
-
 def getOptions(args):
     parser = argparse.ArgumentParser(description="Options to run a spider worker.")
 
     parser.add_argument("-e", "--entries", type=int, help="Number of entries to generate. Default is 10.", default=10)
     parser.add_argument("-t", "--table", help="Table name. Default is SpiderTaskQueue.", default='SpiderTaskQueue')
     parser.add_argument("-d", "--database", help="Database name. Default is SpiderTaskQueue.", default='SpiderTaskQueue')
-    parser.add_argument("--test", type=bool, help="Enbales test mode. Test mode generates entries in local MySQL database. Default is False.", default=False)
+    parser.add_argument("--test", type=bool, help="Enbales test mode. Test mode generates entries in local MySQL database. Default is True.", default=True)
     options = parser.parse_args(args)
     return options
 
