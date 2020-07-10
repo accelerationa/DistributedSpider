2b0e0d1968a7a8dc1c81e466f78867ed12b5e706
accelerationa <yongcong.luo@gmail.com>
2020-07-09 18:01:54 -0700

    Adding test entry generator for mongo

--- src/test_task_entry_generator.py
+++ src/test_task_entry_generator.py
@@ -2,14 +2,18 @@ import time
 from init_mysql_connector_cursor import init_mysql_connector_and_cursor
 import uuid
 from mysql_dao import TaskDBMySqlDao
+from mongo_dao import TaskDBMongoDao
 import argparse
 import sys
 
 # This method generates task entries with (url, 'New', timestamp) format.
 # Make sure database and table exists before calling this method.
-def generate_test_entries(num_entries, database, table, use_local_database):
+def generate_test_entries(num_entries, database, table, use_local_database, db_type):
 
-    dao = TaskDBMySqlDao(database=database, table=table, use_local_database=use_local_database)
+    if db_type == 'mongo':
+        dao = TaskDBMongoDao(database_name=database, collection_name=table, use_local_database=use_local_database)
+    else:
+        dao = TaskDBMySqlDao(database=database, table=table, use_local_database=use_local_database)
     for i in range(num_entries):
         dao.newTask(url=str(uuid.uuid4()))
 
@@ -20,9 +24,10 @@ def getOptions(args):
     parser.add_argument("-t", "--table", help="Table name. Default is SpiderTaskQueue.", default='SpiderTaskQueue')
     parser.add_argument("-d", "--database", help="Database name. Default is SpiderTaskQueue.", default='SpiderTaskQueue')
     parser.add_argument("--test", type=bool, help="Enbales test mode. Test mode generates entries in local MySQL database. Default is True.", default=True)
+    parser.add_argument("-b", "--db_type", type=str, help="Which database type to use as a task queue. Options are (mysql, mongo). Defaults to mongo.", default='mongo')
     options = parser.parse_args(args)
     return options
 
 if __name__ == "__main__":
     options = getOptions(sys.argv[1:])
-    generate_test_entries(database=options.database, table=options.table, use_local_database=options.test, num_entries=options.entries)
+    generate_test_entries(database=options.database, table=options.table, use_local_database=options.test, num_entries=options.entries, db_type=options.db_type)
